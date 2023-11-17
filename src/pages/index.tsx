e5cb9eec9ee4c524e8d21f746119c32e905990b2
Greg Lamp <lamp.greg@gmail.com>
2023-11-17 06:21:23 -0700

    adj

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,13 +4,14 @@ import { sample } from "lodash";
 import Link from "next/link";
 import * as React from "react";
 
-const adjectives = ["fuzzy", "furry", "fluffy", "fierce", "fancy", "fickle"];
+const adjectives1 = ["wild", "wacky", "wonderful", "wicked", "wavy", "wobbly"];
+const adjectives2 = ["fuzzy", "furry", "fluffy", "fierce", "fancy", "fickle"];
 const animals = avatars.map((avatar) => avatar.name);
 
 export default function Home() {
   const [roomLink, setRoomLink] = React.useState<string | undefined>();
   React.useEffect(() => {
-    const roomId = `${sample(adjectives)}-${sample(
+    const roomId = `${sample(adjectives1)}-${sample(adjectives2)}-${sample(
       animals
     )}`.toLocaleLowerCase();
     setRoomLink(`room/${roomId}`);
